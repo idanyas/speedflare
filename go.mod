b7d4042873d2e2ba1eb2f05e34933d2e1acff0b2
idanya <danhilkau2@gmail.com>
2025-03-23 21:17:15 +0300

    feat: integrated dns over https

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/idanyas/speedflare
 
-go 1.20
+go 1.22.0
+
+toolchain go1.24.1
 
 require (
 	github.com/fatih/color v1.15.0
@@ -8,7 +10,15 @@ require (
 )
 
 require (
+	golang.org/x/mod v0.23.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/sync v0.11.0 // indirect
+	golang.org/x/tools v0.30.0 // indirect
+)
+
+require (
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/miekg/dns v1.1.64
+	golang.org/x/sys v0.30.0 // indirect
 )
--- internal/client/client.go
+++ internal/client/client.go
@@ -1,13 +1,20 @@
+// internal/client/client.go
 package client
 
 import (
 	"context"
+	"crypto/tls"
+	"encoding/base64"
+	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"time"
+
+	"github.com/miekg/dns"
 )
 
-func NewHTTPClient(ipv4 bool, ipv6 bool) *http.Client {
+func NewHTTPClient(ipv4, ipv6 bool) *http.Client {
 	dialer := &net.Dialer{
 		Timeout:   30 * time.Second,
 		KeepAlive: 30 * time.Second,
@@ -20,7 +27,50 @@ func NewHTTPClient(ipv4 bool, ipv6 bool) *http.Client {
 			} else if ipv6 {
 				network = "tcp6"
 			}
-			return dialer.DialContext(ctx, network, addr)
+
+			host, port, err := net.SplitHostPort(addr)
+			if err != nil {
+				return nil, err
+			}
+
+			if ip := net.ParseIP(host); ip != nil {
+				return dialer.DialContext(ctx, network, net.JoinHostPort(host, port))
+			}
+
+			var dnsTypes []uint16
+			switch network {
+			case "tcp4":
+				dnsTypes = []uint16{dns.TypeA}
+			case "tcp6":
+				dnsTypes = []uint16{dns.TypeAAAA}
+			default:
+				dnsTypes = []uint16{dns.TypeA, dns.TypeAAAA}
+			}
+
+			ips, err := resolveWithDoH(ctx, host, dnsTypes)
+			if err != nil {
+				return nil, err
+			}
+
+			var firstErr error
+			for _, ip := range ips {
+				if (network == "tcp4" && ip.To4() == nil) || (network == "tcp6" && ip.To4() != nil) {
+					continue
+				}
+
+				conn, err := dialer.DialContext(ctx, network, net.JoinHostPort(ip.String(), port))
+				if err == nil {
+					return conn, nil
+				}
+				if firstErr == nil {
+					firstErr = err
+				}
+			}
+
+			if firstErr != nil {
+				return nil, firstErr
+			}
+			return nil, fmt.Errorf("no IP addresses found for %s", host)
 		},
 		MaxIdleConns:       100,
 		IdleConnTimeout:    90 * time.Second,
@@ -32,4 +82,71 @@ func NewHTTPClient(ipv4 bool, ipv6 bool) *http.Client {
 		Transport: transport,
 		Timeout:   30 * time.Second,
 	}
+}
+
+func resolveWithDoH(ctx context.Context, host string, dnsTypes []uint16) ([]net.IP, error) {
+	var ips []net.IP
+
+	for _, dnsType := range dnsTypes {
+		m := new(dns.Msg)
+		m.SetQuestion(dns.Fqdn(host), dnsType)
+		m.RecursionDesired = true
+
+		msg, err := m.Pack()
+		if err != nil {
+			return nil, err
+		}
+
+		b64 := base64.RawURLEncoding.EncodeToString(msg)
+		url := fmt.Sprintf("https://1.1.1.1/dns-query?dns=%s", b64)
+
+		req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
+		if err != nil {
+			return nil, err
+		}
+		req.Host = "cloudflare-dns.com"
+		req.Header.Set("Accept", "application/dns-message")
+
+		transport := &http.Transport{
+			TLSClientConfig: &tls.Config{
+				ServerName: "cloudflare-dns.com",
+			},
+		}
+		client := &http.Client{Transport: transport}
+
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("DoH request failed with status: %s", resp.Status)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+
+		response := new(dns.Msg)
+		if err := response.Unpack(body); err != nil {
+			return nil, err
+		}
+
+		for _, ans := range response.Answer {
+			switch a := ans.(type) {
+			case *dns.A:
+				ips = append(ips, a.A)
+			case *dns.AAAA:
+				ips = append(ips, a.AAAA)
+			}
+		}
+	}
+
+	if len(ips) == 0 {
+		return nil, fmt.Errorf("no IP addresses found for %s", host)
+	}
+
+	return ips, nil
 }
